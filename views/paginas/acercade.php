e773deeb5042a6436a2890e044785e1076e3fe96
Joseangvas <developer.javp@gmail.com>
2024-02-07 17:05:00 -0400

    Codificar Acerca de

---
+++ views/paginas/acercade.php
@@ -0,0 +1,21 @@
+<main class="shaddai">
+  <h2 class="shaddai__heading"><?php echo $titulo; ?></h2>
+  <p class="shaddai__descripcion">Examenes de la Vista - Monturas</p>
+
+  <div class="shaddai__grid">
+    <div <?php aos_animacion(); ?> class="shaddai__imagen">
+      <picture>
+        <source srcset = "build/img/sobre_shaddai.avif" type="image/avif">
+        <source srcset = "build/img/sobre_shaddai.webp" type="image/webp">
+        <img loading="lazy" width="200" height="400" src="build/img/sobre_shaddai.jpg" alt="Imagen Shaddai">
+      </picture>
+    </div>
+
+    <div class="shaddai__contenido">
+      <p <?php aos_animacion(); ?> class="shaddai__texto">
+      Somos una óptica con más de 20 años de experiencia en el cuidado de la salud visual de nuestros clientes. Ofrecemos servicios de calidad, como exámenes de la vista realizados por profesionales cualificados, y monturas de lentes personalizadas y adaptadas a las necesidades de cada persona. Contamos con una amplia variedad de modelos y marcas de lentes, tanto graduadas como de sol, para todos los gustos y estilos. Nuestra misión es brindar una atención personalizada y satisfacer las expectativas de nuestros clientes, ofreciéndoles soluciones óptimas para mejorar su visión y su calidad de vida.</p>
+
+      <p <?php aos_animacion(); ?> class="shaddai__texto">Nuestra visión es ser una óptica de referencia en el sector, reconocida por su excelencia, innovación y compromiso social. Nuestros valores son la honestidad, la responsabilidad, el respeto, la confianza y la pasión por nuestro trabajo. Te invitamos a visitarnos y a comprobar por ti mismo la diferencia de nuestra óptica. Estaremos encantados de atenderte y de ayudarte a cuidar de tu salud visual.</p>
+    </div>
+  </div>
+</main>
