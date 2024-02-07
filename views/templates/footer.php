e2c5ff3042c6215be145e06a841419f1663c3402
Joseangvas <developer.javp@gmail.com>
2024-02-07 17:13:58 -0400

    Codificar Pie de Pagina

---
+++ views/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+  <div class="footer__grid">
+    <div class="footer__contenido">
+      <h3 class="footer__logo">
+        Servicio Optico Shaddai
+      </h3>
+
+      <p class="footer__texto">
+        Bienvenido a nuestra óptica, donde cuidamos de tu salud visual con profesionalidad y calidad. Somos expertos en exámenes de la vista y monturas de lentes personalizadas, y te ofrecemos una gran variedad de modelos y marcas de lentes graduadas y de sol. Nuestro objetivo es brindarte una atención personalizada y satisfacer tus necesidades visuales, mejorando tu calidad de vida. Visítanos y comprueba la diferencia de nuestra óptica.
+      </p>
+    </div>
+
+    <nav class="menu-redes"> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/joseangvas">
+          <span class="menu-redes__ocultar">Facebook</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/joseangvas_dev">
+          <span class="menu-redes__ocultar">Twitter</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/codejavtech">
+          <span class="menu-redes__ocultar">YouTube</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/joseangvas/?hl=es">
+          <span class="menu-redes__ocultar">Instagram</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@joseangvas">
+          <span class="menu-redes__ocultar">Tiktok</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/joseangvas">
+          <span class="menu-redes__ocultar">Github</span>
+      </a>
+    </nav>
+  </div>
+
+  <p class="footer__copyright">
+    OpticaShaddai
+    <span class="footer__copyright--regular">
+      © Todos los derechos reservados <?php echo date('Y'); ?> | Diseñado por: <a href="#" target="_blank">José Angel Vásquez</a>
+    </span>
+  </p>
+</footer>
