cf8080ab0d34f527284b0f6c3ef50f5b23484f05
Joseangvas <developer.javp@gmail.com>
2024-02-07 17:03:55 -0400

    Codificar Registro de Password

---
+++ views/auth/registro.php
@@ -0,0 +1,74 @@
+<main class="auth">
+  <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+  <p class="auth__texto">Regístrate en la Aplicación</p>
+
+  <?php
+    require_once __DIR__ . '/../templates/alertas.php';
+  ?>
+
+  <form action="/registro" class="formulario" method="POST">
+    <div class="formulario__campo">
+      <label for="nombre" class="formulario__label">Nombre</label>
+      <input
+        type="text"
+        class="formulario__input"
+        placeholder="Tu Nombre"
+        id="nombre"
+        name="nombre"
+        value="<?php echo $usuario->nombre; ?>"
+        />
+      </div>
+      
+      <div class="formulario__campo">
+        <label for="apellido" class="formulario__label">Apellido</label>
+        <input
+        type="text"
+        class="formulario__input"
+        placeholder="Tu Apellido"
+        id="apellido"
+        name="apellido"
+        value="<?php echo $usuario->apellido; ?>"
+        />
+      </div>
+      
+      <div class="formulario__campo">
+        <label for="email" class="formulario__label">Email</label>
+        <input
+        type="email"
+        class="formulario__input"
+        placeholder="Tu Email"
+        id="email"
+        name="email"
+        value="<?php echo $usuario->email; ?>"
+      />
+    </div>
+
+    <div class="formulario__campo">
+      <label for="password" class="formulario__label">Password</label>
+      <input
+        type="password"
+        class="formulario__input"
+        placeholder="Tu Password"
+        id="password"
+        name="password"
+      />
+    </div>
+
+    <div class="formulario__campo">
+      <label for="password" class="formulario__label">Repetir Password</label>
+      <input
+        type="password"
+        class="formulario__input"
+        placeholder="Repetir Password"
+        id="password2"
+        name="password2"
+      />
+    </div>
+    <input type="submit" class="formulario__submit" value="Crear Cuenta">
+  </form>
+
+  <div class="acciones">
+    <a href="/login" class="acciones__enlace">¿Ya Tienes Cuenta? Iniciar Sesión</a>
+    <a href="/olvide" class="acciones__enlace">¿Olvidaste Tu Password?</a>
+  </div>
+</main>
