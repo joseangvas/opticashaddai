d211bdb1da5aa5b3ce02229cde1ff56ae8daa018
Joseangvas <developer.javp@gmail.com>
2024-02-07 17:02:22 -0400

    Codificar Mensaje de Auth

---
+++ views/auth/mensaje.php
@@ -0,0 +1,6 @@
+<main class="auth">
+  <h2 class="auth__heading"><?php echo $titulo; ?></h2>
+  <p class="auth__texto">
+    Es Necesario Confirmar tu Cuenta. Revisa tu Bandeja de Entrada para hacerlo.
+  </p>
+</main>
